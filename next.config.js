c899d79b36541d756a4c2184e7839bffb8498c31
Petar Mirceski <petar.mirceski1998@gmail.com>
2022-01-04 13:05:52 +0900

    error fix

--- next.config.js
+++ next.config.js
@@ -9,16 +9,4 @@ module.exports = withContentlayer()({
   images: {
     domains: ['img.youtube.com'],
   },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
 });
