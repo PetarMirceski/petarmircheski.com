a26c623c7116e2ddda6b6a0296db8581782c46bf
Mircheski Petar <petar.mirceski1998@gmail.com>
2023-10-11 17:10:39 +0900

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "@fontsource/roboto/400.css";
 import { Box } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { MDXProvider } from "@mdx-js/react";
+import { Analytics } from "@vercel/analytics/react";
 import { Footer } from "components/Footer";
 import { Navbar } from "components/Header";
 import { MDXcomponents } from "components/MDXcomponents";
@@ -14,21 +15,24 @@ import { theme } from "styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <AnimatePresence
-        mode="wait"
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <MDXProvider components={MDXcomponents}>
-          <Navbar />
-          <Box as="main" pt={{ base: 16, md: 32 }} pb={{ base: 24, md: 16 }}>
-            <Component {...pageProps} />
-            <Footer />
-          </Box>
-        </MDXProvider>
-      </AnimatePresence>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <AnimatePresence
+          mode="wait"
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <MDXProvider components={MDXcomponents}>
+            <Navbar />
+            <Box as="main" pt={{ base: 16, md: 32 }} pb={{ base: 24, md: 16 }}>
+              <Component {...pageProps} />
+              <Footer />
+            </Box>
+          </MDXProvider>
+        </AnimatePresence>
+      </ChakraProvider>
+      <Analytics />
+    </>
   );
 }
 
