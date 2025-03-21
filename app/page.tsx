60af9e315b4aab0b5d362d0414f28b3dfd2c54f2
Petar Mirceski <petar.mirceski1998@gmail.com>
2025-03-21 17:00:56 +0900

    Update profile image and enhance layout for better presentation

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,19 @@ export default function Home() {
   return (
     <>
       <section className="flex flex-col items-center">
-        <Image
-          alt="Profile"
-          className="size-64 rounded-full"
-          src="/images/beach_crop.jpeg"
-          style={{
-            aspectRatio: "200/200",
-            objectFit: "cover",
-          }}
-          height="200"
-          width="200"
-        />
+        <div className="relative size-[350px] overflow-hidden rounded-full">
+          <Image
+            alt="Profile"
+            src="/images/profile.jpeg"
+            fill
+            style={{
+              objectFit: "cover",
+              objectPosition: "0% 4%", // moved a little to the right
+              // First zoom (scale), then shift right with translateX
+              transform: "scale(1.4) translateX(-6%)",
+            }}
+          />
+        </div>
       </section>
 
       <section className="container mx-auto max-w-7xl px-10">
